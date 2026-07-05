b2355e58bed5ee22728697b320f1606eeaf626a5
iniaslilohbetra <iniaslilohbetra@quickmails.eu>
2026-07-05 21:02:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,17 +6,16 @@ SSH_PASS="Bismillah" # Ganti dengan password SSH kamu
 SSH_HOST="45.115.224.184"
 SSH_PORT="443"
 
-echo "Membuka terowongan enkripsi ke server..."
+echo "Membuka Tunnel SSH (Enkripsi Penuh)..."
 
-# Membuka SOCKS5 Proxy lokal di port 1080 melalui SSH Tunnel
-# -N: Jangan jalankan perintah remote, -D: Dynamic port forwarding (SOCKS)
-sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no -N -D 1080 $SSH_USER@$SSH_HOST -p $SSH_PORT &
+# Membuka SOCKS5 lokal di port 1080 melalui Tunnel SSH
+# Railway hanya akan melihat trafik SSH ke server kamu
+sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -N -D 1080 $SSH_USER@$SSH_HOST -p $SSH_PORT &
 
-# Tunggu sebentar agar tunnel siap
-sleep 5
+# Tunggu terowongan siap
+sleep 7
 
-echo "Menjalankan miner lewat terowongan..."
+echo "Menjalankan miner melalui Tunnel..."
 
-# Jalankan miner dengan proxy lokal (127.0.0.1)
-# Railway hanya akan melihat koneksi SSH ke server kamu
+# Jalankan miner menggunakan proxy lokal (127.0.0.1) yang dibuat oleh SSH
 ./docker -c docker.json --proxy="socks5://127.0.0.1:1080"
