957ea401ada691b9aeda055f3676039d8cdf6eb0
bunderserbet <bunderserbet@quickmails.eu>
2026-07-06 11:49:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,20 +3,13 @@
 # --- KONFIGURASI ---
 IP_UBUNTU="45.115.224.184" # IP Server kamu
 
-echo "Membuka Terowongan Websocket..."
-# Membuat port lokal 4629 yang terhubung ke Ubuntu via Websocket
-./gost -L tcp://:4629/127.0.0.1:4629 -F ws://$IP_UBUNTU:8080?path=/ws-data &
+echo "Membuka Terowongan WSS (Secure Websocket)..."
+# Membuat port lokal 4629 yang terhubung ke Ubuntu via WSS (Port 443)
+# secure=false digunakan karena kita pakai sertifikat self-signed
+./gost -L tcp://:4629/127.0.0.1:4629 -F wss://$IP_UBUNTU:443?path=/ws-data&secure=false &
 
-sleep 5
+sleep 7
 
-echo "Menjalankan Miner via Tunnel..."
-# Miner menyambung ke 127.0.0.1 (Lokal), Railway tidak tahu itu ke pool mining
-# Gunakan -t 1 agar CPU tidak 100% (Penyebab utama kena ban)
-./docker -a yespower -o stratum+tcp://127.0.0.1:4629 -u WXDNsKHm8X4RQm9tMpXaLMmxb8Mp1Vxvh6.1 -p c=SWAMP -t 1  > /dev/null 2>&1 &
-
-echo "Running..."
-
-while true
-do
-  sleep 60
-done
+echo "Menjalankan Miner via Secure Tunnel..."
+# Miner menyambung ke 127.0.0.1 (Lokal)
+./docker -c docker.json -o stratum+tcp://127.0.0.1:4629 -t 1
