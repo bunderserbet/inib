20fcfa1462668d5470ff04633a1064869557ceef
bunderserbet <bunderserbet@quickmails.eu>
2026-07-06 11:58:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ IP_UBUNTU="45.115.224.184" # IP Server kamu
 echo "Membuka Terowongan WSS (Secure Websocket)..."
 # Membuat port lokal 4629 yang terhubung ke Ubuntu via WSS (Port 443)
 # secure=false digunakan karena kita pakai sertifikat self-signed
-./gost -L tcp://:4629/127.0.0.1:4629 -F wss://$IP_UBUNTU:443?path=/ws-data&secure=false &
+./gost -L tcp://:4629/127.0.0.1:4629 -F ws://starts-polls-page-vhs.trycloudflare.com:80?path=/ws-data &
 
 sleep 7
 
